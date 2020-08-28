69440b74d7da447c22157b93fb2e137c2ac3c320
de-souza <leo@de-souza.eu>
2020-08-28 21:29:33 +0200

    Create website sructure

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Calculate ETH Emissions | carbon.fyi</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to carbon.fyi
+        </h1>
+
+        <p>
+          In progress...
+        </p>
+      </main>
+    </>
+  )
+}
