aeb32df9bb36177fdfe76d628be6cc3c3bdaff01
de-souza <leo@de-souza.eu>
2020-08-30 20:44:28 +0200

    Also use the FC syntax for the custom App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,14 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Calculate ETH Emissions | carbon.fyi</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Calculate ETH Emissions | carbon.fyi</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
