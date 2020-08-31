cb850bc5ae14b5180d44b8c51cf7a2bb3710445b
de-souza <leo@de-souza.eu>
2020-08-31 19:31:28 +0200

    Add "under construction" pages

---
+++ pages/stats.tsx
@@ -0,0 +1,12 @@
+import Banner from "components/Banner";
+import Logo from "components/Logo";
+
+export default function Offset() {
+  return (
+    <>
+      <Banner />
+      <main><p>Under construction...</p></main>
+      <Logo />
+    </>
+  );
+}
