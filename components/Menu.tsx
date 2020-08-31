6c90643279dc39ab05c1bf305112cc9a14bcd741
de-souza <leo@de-souza.eu>
2020-08-31 12:43:39 +0200

    Simplify Menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,12 +1,19 @@
-import MenuItem, { MenuItemProps } from "components/MenuItem";
+import Link from "next/link";
 
-interface MenuProps {
-  menuItemsProps: Array<MenuItemProps>;
+interface ItemProps {
+  text: string;
+  link: string;
+  color: string;
+  svg: React.ComponentType;
 }
 
-export default function Menu({ menuItemsProps }: MenuProps) {
-  const menuItems = menuItemsProps.map(({ text, link, color }) => (
-    <MenuItem key={text} text={text} link={link} color={color} />
+interface MenuProps {
+  itemProps: ItemProps[];
+}
+
+export default function Menu({ itemProps }: MenuProps) {
+  const menuItems = itemProps.map(({ text, link, color, svg }) => (
+    <MenuItem key={text} text={text} link={link} color={color} svg={svg} />
   ));
   return (
     <>
@@ -27,6 +34,42 @@ export default function Menu({ menuItemsProps }: MenuProps) {
             font-size: 16vw;
             flex-flow: column;
           }
+        }
+      `}</style>
+    </>
+  );
+}
+
+function MenuItem(props: ItemProps) {
+  return (
+    <>
+      <li>
+        <Link href={props.link}>
+          <a>
+            {props.text}
+            {/* <props.svg /> */}
+          </a>
+        </Link>
+      </li>
+      <style jsx>{`
+        li {
+          flex: 1;
+        }
+
+        a {
+          text-decoration: none;
+          color: white;
+          display: flex;
+          flex-flow: column;
+          align-items: center;
+          justify-content: center;
+          height: 100%;
+          background: ${props.color};
+        }
+
+        a:hover {
+          background: white;
+          color: ${props.color};
         }
       `}</style>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import Menu from "components/Menu";
+import About from "components/svg/About";
 
-const menuItemsProps = [
-  { text: "stats", link: "/stats", color: "#b3e93f" },
-  { text: "offset", link: "/offset", color: "#743ee8" },
-  { text: "about", link: "/about", color: "#fe7c6f" },
-];
+const statsProps = {
+  text: "stats",
+  link: "/stats",
+  color: "#b3e93f",
+  svg: About,
+};
+
+const offsetProps = {
+  text: "offset",
+  link: "/offset",
+  color: "#743ee8",
+  svg: About,
+};
+
+const aboutProps = {
+  text: "about",
+  link: "/about",
+  color: "#fe7c6f",
+  svg: About,
+};
 
 export default function Home() {
-  return <Menu menuItemsProps={menuItemsProps} />;
+  return <Menu itemProps={[statsProps, offsetProps, aboutProps]} />;
 }
