471cad0bab9d0a82e5cce7ef6c5fe55b5119abb9
de-souza <leo@de-souza.eu>
2020-08-31 03:32:50 +0200

    Separate global CSS into Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+export default function Layout(props: { children?: any }) {
+  return (
+    <>
+      {props.children}
+      <style jsx global>{`
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+          min-height: 100vh;
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Layout from "components/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,16 +9,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Calculate ETH Emissions | carbon.fyi</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
-      <style jsx global>{`
-        body {
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
-          min-height: 100vh;
-          margin: 0;
-          padding: 0;
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
