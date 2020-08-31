3398096e38fd958f6318883422acf7083c7c318e
de-souza <leo@de-souza.eu>
2020-08-31 21:01:05 +0200

    Make form responsive

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,12 +1,45 @@
 export default function Input() {
   return (
     <form>
-      <label>
-        <p>
-          Please enter an ETH address:
+      <ul>
+        <li>
+          <label>Please enter an ETH address:</label>
           <input type="text" name="a"></input>
-        </p>
-      </label>
+        </li>
+        <li>
+          <button type="submit">Analyze</button>
+        </li>
+      </ul>
+      <style jsx>{`
+        form {
+          margin: 0.5rem auto;
+        }
+
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
+
+        li {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          margin: 0.5rem 0;
+        }
+
+        label {
+          flex-basis: 16rem;
+        }
+
+        input {
+          flex: 1;
+        }
+
+        button {
+          margin: auto;
+        }
+      `}</style>
     </form>
   );
 }
