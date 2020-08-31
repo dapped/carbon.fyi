153789419df1b875932323bc5aa50afbb3d221a9
de-souza <leo@de-souza.eu>
2020-08-31 20:51:29 +0200

    Add global line height

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ export default function Layout(props: { children?: any }) {
         body {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+          line-height: 1.6;
           min-height: 100vh;
           margin: 0;
           padding: 0;
