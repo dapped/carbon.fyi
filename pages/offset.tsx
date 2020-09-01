bedeaf17101dc499bda7f3b48b37fc306ac1c2f9
de-souza <leo@de-souza.eu>
2020-09-01 19:37:46 +0200

    Harmonize HTML markup

--- pages/offset.tsx
+++ pages/offset.tsx
@@ -1,23 +1,24 @@
 import { useRouter } from "next/router";
 import Banner from "components/Banner";
-import Wrapper from "components/Wrapper";
 import Border from "components/Border";
 import Emissions from "components/Emissions";
 import Input from "components/Input";
 import Logo from "components/Logo";
+import Wrapper from "components/Wrapper";
 
 export default function Offset() {
   const router = useRouter();
   const address = router.query.a?.toString();
-
   return (
     <>
       <Banner />
-      <Wrapper>
-        <Border>
-          <main>{address ? <Emissions address={address} /> : <Input />}</main>
-        </Border>
-      </Wrapper>
+      <main>
+        <Wrapper>
+          <Border>
+            {address ? <Emissions address={address} /> : <Input />}
+          </Border>
+        </Wrapper>
+      </main>
       <Logo />
     </>
   );
