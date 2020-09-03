d1c0fe387a43ae73423a699a4c2c3542e9c927cd
de-souza <leo@de-souza.eu>
2020-09-03 17:28:15 +0200

    Simplify Emissions module

--- components/Emissions.tsx
+++ components/Emissions.tsx
@@ -1,105 +1,32 @@
 import useSWR from "swr";
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-const KGCO2_PER_GAS = 0.0003100393448;
-
-interface EmissionsProps {
-  address: string;
-}
-
-interface TransactionData {
+interface Transaction {
   from: string;
   gas: number;
 }
 
-export default function Emissions({ address }: EmissionsProps) {
-  const api_url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${API_KEY}`;
+export default function Emissions({ address }: { address: string }) {
+  const api_key = process.env.NEXT_PUBLIC_API_KEY;
+  const api_url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${api_key}`;
   const { data, error } = useSWR(api_url, fetcher);
 
-  if (error)
-    return (
-      <p>
-        Error fetching data.
-        <style jsx>{`
-          p {
-            text-align: center;
-          }
-        `}</style>
-      </p>
-    );
+  let text;
+  if (error) text = ["Error fetching data."];
+  else if (!data) text = ["Fetching data..."];
+  else if (data.status == 0) text = [data.result];
+  else text = emissionsText(address, data.result);
 
-  if (!data)
-    return (
-      <p>
-        Fetching data...
-        <style jsx>{`
-          p {
-            text-align: center;
-          }
-        `}</style>
-      </p>
-    );
-
-  if (data.status == 0)
-    return (
-      <p>
-        {data.result}
-        <style jsx>{`
-          p {
-            text-align: center;
-          }
-        `}</style>
-      </p>
-    );
-
-  const sent = data.result.filter(
-    (cur: TransactionData) => cur.from == address.toLowerCase()
-  );
-
-  const transactions = sent.length;
-  const gas = getGas(sent);
-  const kgco2 = Math.round(gas * KGCO2_PER_GAS);
-
-  return (
+  const paragraphs = text.map((paragraph: string) => (
     <>
-      {transactions == 0 ? (
-        <p>
-          0 transactions were sent from this address.
-        </p>
-      ) : (
-        <>
-          {transactions == 1 ? (
-            <>
-              <p>
-                1 transaction was sent from this address.
-              </p>
-              <p>This transaction consumed {gas} gas.</p>
-            </>
-          ) : (
-            <>
-              <p>
-                {transactions} transactions were sent from this address.
-              </p>
-              <p>These transactions consumed {gas} gas.</p>
-            </>
-          )}
-          <p>
-            This emitted the equivalent of {kgco2} kg of CO₂ into the
-            atmosphere.
-          </p>
-          <p>
-            Offset this now at{" "}
-            <a href="https://www.offsetra.com/">Offsetra.com</a>.
-          </p>
-        </>
-      )}
+      <p>{paragraph}</p>
       <style jsx>{`
         p {
           text-align: center;
         }
       `}</style>
     </>
-  );
+  ));
+  return paragraphs;
 }
 
 async function fetcher(url: string) {
@@ -107,10 +34,38 @@ async function fetcher(url: string) {
   return await res.json();
 }
 
-function gasSum(acc: number, cur: TransactionData) {
+function emissionsText(address: string, data: Array<Transaction>) {
+  const kgco2_per_gas = 0.0003100393448;
+
+  const emittedData = data.filter(
+    (cur: Transaction) => cur.from == address.toLowerCase()
+  );
+  const emittedNum = emittedData.length;
+  const gas = consumedGas(emittedData);
+  const kgco2 = Math.round(gas * kgco2_per_gas);
+
+  let text = [];
+  if (emittedNum == 0) {
+    text.push("0 transactions were sent from this address.");
+  } else {
+    if (emittedNum == 1) {
+      text.push("1 transaction was sent from this address.");
+      text.push(`This transaction consumed ${gas} gas.`);
+    } else {
+      text.push(`${emittedNum} transactions were sent from this address.`);
+      text.push(`These transactions consumed ${gas} gas.`);
+    }
+    text.push(
+      `This emitted the equivalent of ${kgco2} kg of CO₂ into the atmosphere.`
+    );
+  }
+  return text;
+}
+
+function gasSum(acc: number, cur: Transaction) {
   return acc + Number(cur.gas);
 }
 
-function getGas(res: Array<TransactionData>) {
+function consumedGas(res: Transaction[]) {
   return res.reduce(gasSum, 0);
 }
