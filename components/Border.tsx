5b34f87cf29861bf736ce73061b83109a47c0248
Brendan <brendanmc6@gmail.com>
2020-09-06 12:15:59 -0700

    remove Any types

--- components/Border.tsx
+++ components/Border.tsx
@@ -1,17 +1,17 @@
 import { colors } from "lib/theme";
+import { FC } from "react";
 
-export default function Border(props: { children?: any }) {
-  return (
-    <div>
-      {props.children}
-      <style jsx>{`
-        div {
-          border: 0.2rem solid ${colors.offsetraPurple};
-          border-radius: 1.5rem;
-          margin: 1rem 2rem;
-          padding: 1rem;
-        }
-      `}</style>
-    </div>
-  );
-}
+const Border: FC = ({ children }) => (
+  <div>
+    {children}
+    <style jsx>{`
+      div {
+        border: 0.2rem solid ${colors.offsetraPurple};
+        border-radius: 1.5rem;
+        margin: 1rem 2rem;
+        padding: 1rem;
+      }
+    `}</style>
+  </div>
+);
+export default Border;
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,15 +1,17 @@
-export default function Main(props: { children?: any }) {
-  return (
-    <main>
-      {props.children}
-      <style jsx>{`
-        main {
-          flex: auto;
-          align-self: center;
-          width: 100%;
-          max-width: 50rem;
-        }
-      `}</style>
-    </main>
-  );
-}
+import { FC } from "react";
+
+const Main: FC = ({ children }) => (
+  <main>
+    {children}
+    <style jsx>{`
+      main {
+        flex: auto;
+        align-self: center;
+        width: 100%;
+        max-width: 50rem;
+      }
+    `}</style>
+  </main>
+);
+
+export default Main;
