e1de3c753e548b3ee5f5e812cfc59e4538c9438f
Brendan McGill <brendanmc6@gmail.com>
2020-09-05 17:26:24 -0700

    refactor(Layout): use FC type, relocate  props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,45 +1,28 @@
 import Banner from "components/Banner";
 import Main from "components/Main";
 import Footer from "components/Footer";
+import { FC } from "react";
 
-export default function Layout(props: { children?: any }) {
-  const stats = {
-    text: "stats",
-    link: "/stats",
-    color: "#b3e93f",
-  };
-  const offset = {
-    text: "offset",
-    link: "/",
-    color: "#743ee8",
-  };
-  const about = {
-    text: "about",
-    link: "/about",
-    color: "#fe7c6f",
-  };
-  const bannerProps = [stats, offset, about];
+const Layout: FC = ({ children }) => (
+  <>
+    <Banner />
+    <Main>{children}</Main>
+    <Footer />
+    <style jsx global>{`
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+        line-height: 1.6;
+        margin: 0;
+        padding: 0;
+      }
 
-  return (
-    <>
-      <Banner bannerProps={bannerProps} />
-      <Main>{props.children}</Main>
-      <Footer />
-      <style jsx global>{`
-        body {
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
-          line-height: 1.6;
-          margin: 0;
-          padding: 0;
-        }
-
-        #__next {
-          display: flex;
-          flex-flow: column;
-          min-height: 100vh;
-        }
-      `}</style>
-    </>
-  );
-}
+      #__next {
+        display: flex;
+        flex-flow: column;
+        min-height: 100vh;
+      }
+    `}</style>
+  </>
+);
+export default Layout;
